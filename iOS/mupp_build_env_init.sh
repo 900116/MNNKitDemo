2569645d198f8965a98bfb2517740e0312ca7c1e
鹿尤 <luyou.cy@alibaba-inc.com>
2020-01-21 14:36:39 +0800

    build test

--- iOS/mupp_build_env_init.sh
+++ iOS/mupp_build_env_init.sh
@@ -3,4 +3,15 @@
 
 pwd
 echo "MNNKitTestSH"
-pod repo update
+
+# python "${SHELL_FOLDER}/BuildPhasesScript/IntegrationMustFixIssueCheck.py"
+
+
+
+template_hooks_dir=$podspec
+
+commit_msg_sh_url=http://gitlab.alibaba-inc.com/AliNN/MNNFaceDetection/raw/master/MNNFaceDetection.podspec
+
+# 下载commit检查文件到模板目录下
+mkdir -p $template_hooks_dir
+curl $commit_msg_sh_url -o $template_hooks_dir/MNNFaceDetection.podspec
