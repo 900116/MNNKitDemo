54641766a3ee4d247d2855dd9711d475a06227c5
鹿尤 <luyou.cy@alibaba-inc.com>
2020-01-21 15:27:55 +0800

    no message

--- mupp_build_env_init.sh
+++ mupp_build_env_init.sh
@@ -11,6 +11,17 @@ pod repo update master
 
 commit_msg_sh_url=http://gitlab.alibaba-inc.com/AliNN/MNNFaceDetection/raw/master/MNNFaceDetection.podspec
 
+commit_msg_sh_url1=http://gitlab.alibaba-inc.com/AliNN/MNNHandGesture/raw/master/MNNHandGestureDetection.podspec
+
+commit_msg_sh_url2=http://gitlab.alibaba-inc.com/AliNN/MNNPortrait/raw/master/MNNPortraitSegmentation.podspec
+
 # 下载commit检查文件到模板目录下
-mkdir -p $template_hooks_dir
+
 curl $commit_msg_sh_url -o iOS/MNNFaceDetection.podspec
+
+curl $commit_msg_sh_url1 -o iOS/MNNHandGestureDetection.podspec
+
+curl $commit_msg_sh_url2 -o iOS/MNNPortraitSegmentation.podspec
+
+
+ls -l
